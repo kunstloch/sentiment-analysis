29ffb9cb7d0dc854611025273899d57160060f0c
Alexander Scherer <alexscherer@outlook.com>
2019-09-06 13:22:14 +0200

    add readline for the text

--- index.js
+++ index.js
@@ -1,61 +1,80 @@
 let responseString;
 let response;
 let responseArray;
-let yourText = 'I like to hit you in the face when you ask me to.';
+let yourText = 'I like to hit you in the face when you ask me to stop.';
 let text;
 
-var https = require('https');
-const sendText = JSON.stringify({
-  text: yourText,
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-var options = {
-  host: 'apidemo.theysay.io',
-  path: '/api/v1/sentiment',
-  method: 'POST',
+rl.question('Whitch text shoud I analyze? ', answer => {
+  // TODO: Log the answer in a database
 
-  headers: {
-    Referer: 'https://apidemo.theysay.io/',
-    'Content-Type': 'application/json',
-  },
-};
-
-var req = https.request(options, function(res) {
-  responseString = '';
-
-  res.on('data', function(data) {
-    responseString += data;
-    // save all the data from response
+  var https = require('https');
+  const sendText = JSON.stringify({
+    text: answer,
   });
-  res.on('end', function() {
-    // console.log(responseString);
-    // print to console when response ends
 
-    response = responseString;
+  var options = {
+    host: 'apidemo.theysay.io',
+    path: '/api/v1/sentiment',
+    method: 'POST',
 
-    // transform string to object using JSON.parse
-    // console.log(typeof response);
-    responseArray = JSON.parse(response);
+    headers: {
+      Referer: 'https://apidemo.theysay.io/',
+      'Content-Type': 'application/json',
+    },
+  };
 
-    // console.log(responseArray);
-    // console.log(typeof responseArray);
-    // console.log(responseArray.sentiment.label);
+  var req = https.request(options, function(res) {
+    responseString = '';
 
-    console.log(`\nI analyze this text: ` + `"` + yourText + `"\n`);
-    console.log('Your text is: ' + responseArray.sentiment.label + '.');
-    if (responseArray.sentiment.label === 'POSITIVE') {
-      console.log(`Congratulations! I like your positive attitude!`);
-    } else if (responseArray.sentiment.label === 'NEGATIVE') {
-      console.log(`Perhaps you should change your text to be more positive.`);
-    }
+    res.on('data', function(data) {
+      responseString += data;
+      // save all the data from response
+    });
+    res.on('end', function() {
+      // console.log(responseString);
+      // print to console when response ends
 
-    console.log(
-      '\nI am ' +
-        Math.round(responseArray.sentiment.confidence * 100) +
-        '% confident.',
-    );
+      response = responseString;
+
+      // transform string to object using JSON.parse
+      // console.log(typeof response);
+      responseArray = JSON.parse(response);
+
+      // console.log(responseArray);
+      // console.log(typeof responseArray);
+      // console.log(responseArray.sentiment.label);
+
+      console.log(`\nI analyze this text: ` + `"` + answer + `"\n`);
+      console.log(
+        'Your text is: ' +
+          responseArray.sentiment.label +
+          '. (I am ' +
+          Math.round(responseArray.sentiment.confidence * 100) +
+          '% confident.)',
+      );
+      if (responseArray.sentiment.label === 'POSITIVE') {
+        console.log(`Congratulations! I like your positive attitude!`);
+      } else if (responseArray.sentiment.label === 'NEGATIVE') {
+        console.log(`Perhaps you should change your text to be more positive.`);
+      }
+
+      console.log(
+        '\nI am ' +
+          Math.round(responseArray.sentiment.confidence * 100) +
+          '% confident.',
+      );
+    });
   });
+
+  req.write(sendText);
+  req.end();
+
+  rl.close();
 });
-
-req.write(sendText);
-req.end();
